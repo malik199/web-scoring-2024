11a26a5a3f0a54fe4700efea385eb709f487536f
Malik Ahmad <malik199@gmail.com>
2024-01-29 16:16:55 -0500

    next change

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,12 +8,32 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import {
+  MatDialog,
+  MatDialogRef,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogTitle,
+  MatDialogContent,
+} from '@angular/material/dialog';
+import {
+  doc,
   Firestore,
   collectionData,
   collection,
   addDoc,
+  setDoc,
+  updateDoc
 } from '@angular/fire/firestore';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Observable } from 'rxjs';
+import {MatTableModule} from '@angular/material/table';
+
+import { TournamentsX } from './../../interfaces/tournament';
 
 @Component({
   selector: 'app-dashboard',
@@ -26,49 +46,117 @@ import { Observable } from 'rxjs';
     MatButtonModule,
     MatIconModule,
     CommonModule,
+    ReactiveFormsModule,
+    MatTableModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
+
 export class DashboardComponent {
+
+  displayedColumns: string[] = ['pin', 'link'];
+  dataSource: any;
+  tournamentId: string = '';
+  myForm!: FormGroup;
+  timestamp = new Date().getTime();
+  collectionInstance = collection(this.firestore, 'tournaments');
+
   pin: string | null = null;
   competitor1: string = '';
   competitor2: string = '';
 
-  item$: Observable<any[]>;
-  //firestore: Firestore = inject(Firestore);
-
-  constructor(private firestore: Firestore) {
-    const itemCollection = collection(this.firestore, 'matches');
-    this.item$ = collectionData(itemCollection);
+  constructor(private firestore: Firestore, public dialog: MatDialog) {
+    this.fetchData();
   }
 
-  generatePin() {
-    this.pin = Math.floor(10000 + Math.random() * 90000).toString();
-    this.item$.subscribe(val => {
-      console.log(val);
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      redCorner: new FormControl('', Validators.required),
+      blueCorner: new FormControl('', Validators.required),
     });
   }
 
-  startGame() {
-    const collectionInstance = collection(this.firestore, 'matches');
-    const myDocumentData = {
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      age: 30
+  startTournament() {
+    this.pin = Math.floor(10000 + Math.random() * 90000).toString();
+    
+    const myTournamentData = {
+      pin: this.pin,
+      judges: {
+        judge1: '',
+        judge2: '',
+        judge3: '',
+      }
     };
-    addDoc(collectionInstance, myDocumentData).then(()=> {
-      console.log('Data Saved Successfully')
-    })
-  ; 
+    addDoc(this.collectionInstance, myTournamentData).then((res) => {
+      this.tournamentId = res.id
+    });
 
-    alert('Game Started');
-    /* if(this.pin) {
-      this.db.object(`tournaments/${this.pin}`).update({
-        competitor1: this.competitor1,
-        competitor2: this.competitor2
-      });
-      // Additional logic to start the game
-    }*/
+    alert('New Tournament Added!');
   }
+
+  fetchData() {
+    const data$: Observable<any[]> = collectionData(this.collectionInstance);
+    data$.subscribe(data => {
+      this.dataSource = data ;
+    });
+  }
+
+  async onSubmit() {
+    if (this.myForm.valid) {
+      const redCorner = this.myForm.get('redCorner')?.value;
+      const blueCorner = this.myForm.get('blueCorner')?.value;
+
+
+      // Example: Updating the first match of a specific tournament
+
+
+      const matchId = this.timestamp; // The ID of the match to update
+      //const updateCollection = collection(this.firestore, 'tournaments/owNZxOVkQ5TwKMaR8goO/matches/blueCorner/competitorName');
+      const matchDocRef = doc(this.firestore, `tournaments/${this.tournamentId}/myMatches/${new Date().getTime()}`);
+      await setDoc(matchDocRef, {
+        redCorner: {
+          competitorName: redCorner,
+          score: 0
+        },
+        blueCorner: {
+          competitorName: blueCorner,
+          score: 0
+        }
+      }).then(res => {
+        console.log(res);
+      })
+    } else {
+      alert('Please fill in both fields.');
+    }
+  }
+
+  openDialog(): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '250px',
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  template: `<h1 mat-dialog-title>New Tournament</h1>
+    <div mat-dialog-content>
+      Would start a new tournament with different game pin?
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button mat-dialog-close>No</button>
+      <button mat-button mat-dialog-close cdkFocusInitial>Ok</button>
+    </div>`,
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+  ],
+})
+export class DialogAnimationsExampleDialog {
+  constructor(public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>) {}
 }
---
+++ src/app/interfaces/tournament.ts
@@ -0,0 +1,5 @@
+export interface TournamentsX {
+    judges: object[];
+    pin: number;
+}
+  
